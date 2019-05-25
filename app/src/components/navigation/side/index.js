dfb98bb21898dbbe6f41335b9c8dba5254d561e3
Dustin Snoap <afrobacon@gmail.com>
2019-05-25 18:40:59 -0400

    created and styled side-nav

--- app/src/components/navigation/side/index.js
+++ app/src/components/navigation/side/index.js
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, {Component} from 'react'
 // import {Link} from 'react-router-dom'
 import Wrapper from './style'
 
-const SideNav = () =>
-    <Wrapper>
-        <header>
-            <div className='heading'>
-                <pre className='host'>http://localhost:3333</pre>
-                <pre className='ver'>ver. 0.001 beta</pre>
+class SideNav extends Component {
+    //toggles 'open' class on tables node when it's header is clicked
+    h_open_table = e => e.currentTarget.nextSibling.classList.toggle('open')
+    render = () =>
+        <Wrapper>
+            <header>
+                <div className='heading'>
+                    <pre className='host'>http://localhost:3333</pre>
+                    <pre className='ver'>ver. 0.001 beta</pre>
+                </div>
+                <div className='stats'>
+                    <pre className='value'>0</pre>
+                    <pre className='title'>Tables</pre>
+                    <pre className='value'>0</pre>
+                    <pre className='title'>Entries</pre>
+                    {/* refresh btn */}
+                </div>
+                <div className='filter'>
+                    <figure>o-</figure>
+                    <input type='text' placeholder='filter'/>
+                </div>
+            </header>
+            <div className='content'>
+                <div className='database'>
+                    <header onClick={this.h_open_table}>
+                        <figure>{'{=}'}</figure>
+                        <pre>dbName</pre>
+                    </header>
+                    <div className='tables'>
+                        <pre className='table'>tableName</pre>
+                        <pre className='table'>tableName</pre>
+                        <pre className='table'>tableName</pre>
+                    </div>
+                </div>
+                <div className='database'>
+                    <header onClick={this.h_open_table}>
+                        <figure>{'{=}'}</figure>
+                        <pre>dbName</pre>
+                    </header>
+                    <div className='tables'>
+                        <pre className='table'>tableName</pre>
+                        <pre className='table'>tableName</pre>
+                        <pre className='table'>tableName</pre>
+                    </div>
+                </div>
             </div>
-            <div className='stats'>
-                <pre className='value'>0</pre>
-                <pre className='title'>Tables</pre>
-                <pre className='value'>0</pre>
-                <pre className='title'>Entries</pre>
-                {/* refresh btn */}
-            </div>
-            {/* filter */}
-        </header>
-    </Wrapper>
+        </Wrapper>
+}   
 
 export default SideNav
--- app/src/components/navigation/side/style.js
+++ app/src/components/navigation/side/style.js
@@ -3,26 +3,72 @@ import Styled from 'styled-components'
 export default Styled.div`
     background-color: #222;
     color: #ccc;
+    display: flex;
+    flex-direction: column;
     height: 100%;
     padding: 1rem;
-
     header {
         display: grid;
         grid-auto-flow: row;
         grid-gap: 1rem;
+        margin-bottom: 1rem;
         .heading {
-            .host {
-                font-size: 1.8rem;
-            }
-            .ver {
-                font-size: 1.6rem;
-            }
+            .host {font-size: 1.8rem}
+            .ver {font-size: 1.6rem}
         }
         .stats {
             display: grid;
-            grid-auto-flow: column;
             font-size: 1.6rem;
+            grid-auto-flow: column;
             .value {font-weight: 1000}
+        }
+        .filter {
+            padding: 0;
+            position: relative;
+            transform: translateX(-1rem);
+            width: calc(100% + 2rem);
+            input {
+                background-color: rgba(250,250,250,.75);
+                padding: 0.5rem;
+                text-indent: 30px;
+                width: 100%;    
+                &:hover {background-color: rgba(250,250,250,.85)}
+            }
+            figure {
+                color: #000;
+                font-size: 2rem;
+                left: 15px;
+                position: absolute;
+            }
+        }
+    }
+    .content {
+        display: grid;
+        font-size: 1.6rem;
+        grid-auto-flow: row;
+        grid-gap: 1rem;
+        height: fit-content;
+        .database {
+            display: grid;
+            grid-auto-flow: row;
+            grid-gap: 0.5rem;
+            overflow: hidden;
+            header {
+                cursor: pointer;
+                display: flex;
+                margin: 0;
+            }
+            .tables {
+                cursor: pointer;
+                display: grid;
+                grid-auto-flow: row;
+                grid-gap: 0.5rem;
+                height: fit-content;
+                max-height: 0;
+                transition: max-height 420ms ease-in-out;
+                &.open {max-height: 200px}
+                .table {text-indent: 40px}
+            }
         }
     }
 `
